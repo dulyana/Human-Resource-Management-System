3492df6e0588d1ffa54c11880c64a084cf7fd38a
Dulyana  Gunawardhana <67815837+dulyana@users.noreply.github.com>
2021-04-01 00:44:35 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROC [dbo].[EmployeeViewAll]
+AS
+SELECT EPFno,EmployeeName,EmpAddress,PostalCode,ContactNumber,NIC,Branch,TaxCode,BasicSalary,Gender,Age,EmpState,DOB FROM EmpDetails
+CREATE TABLE [dbo].[Employee]
+(
+	[Employee_no] NVARCHAR(MAX) NOT NULL PRIMARY KEY,
+	[Employee_name] NVARCHAR(MAX) NULL,
+	[Other_names] NVARCHAR(MAX) NULL,
+	[Address] NVARCHAR(MAX) NULL,
+	[Contact_no] NVARCHAR(MAX) NULL,
+	[Email] NVARCHAR(MAX) NULL,
+	[Designation] NVARCHAR(MAX) NULL,
+	[Department] NVARCHAR(MAX) NULL,
+	[Salary] NVARCHAR(MAX) NULL,
+	[Department_location] NVARCHAR(MAX) NULL
+
+)

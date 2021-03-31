f824fc0636855e96c0ceb83e61b0006715721786
Dulyana  Gunawardhana <67815837+dulyana@users.noreply.github.com>
2021-03-31 23:32:43 +0530

    Add files via upload

---
+++ frmLogin.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace SADProject2
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btn_Login = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chk_Remme = new Bunifu.Framework.UI.BunifuCheckbox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_Min = new Bunifu.Framework.UI.BunifuImageButton();
+            this.bunifuImageButton1 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.btn_Close = new Bunifu.Framework.UI.BunifuImageButton();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_Min)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_Close)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(223)))), ((int)(((byte)(0)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(850, 13);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel2.Controls.Add(this.txtPwd);
+            this.panel2.Controls.Add(this.txtUser);
+            this.panel2.Controls.Add(this.btn_Login);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.chk_Remme);
+            this.panel2.Controls.Add(this.linkLabel1);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(154, 46);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(563, 444);
+            this.panel2.TabIndex = 1;
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.ActiveBorderThickness = 1;
+            this.btn_Login.ActiveCornerRadius = 20;
+            this.btn_Login.ActiveFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(223)))), ((int)(((byte)(0)))));
+            this.btn_Login.ActiveForecolor = System.Drawing.Color.White;
+            this.btn_Login.ActiveLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(223)))), ((int)(((byte)(0)))));
+            this.btn_Login.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_Login.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_Login.BackgroundImage")));
+            this.btn_Login.ButtonText = "Login";
+            this.btn_Login.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Login.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Login.ForeColor = System.Drawing.Color.White;
+            this.btn_Login.IdleBorderThickness = 1;
+            this.btn_Login.IdleCornerRadius = 20;
+            this.btn_Login.IdleFillColor = System.Drawing.Color.Khaki;
+            this.btn_Login.IdleForecolor = System.Drawing.Color.DimGray;
+            this.btn_Login.IdleLineColor = System.Drawing.Color.Khaki;
+            this.btn_Login.Location = new System.Drawing.Point(92, 334);
+            this.btn_Login.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(375, 51);
+            this.btn_Login.TabIndex = 16;
+            this.btn_Login.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semilight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label4.Location = new System.Drawing.Point(238, 308);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 21);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Remember Me?";
+            // 
+            // chk_Remme
+            // 
+            this.chk_Remme.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(223)))), ((int)(((byte)(0)))));
+            this.chk_Remme.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.chk_Remme.ChechedOffColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(135)))), ((int)(((byte)(140)))));
+            this.chk_Remme.Checked = true;
+            this.chk_Remme.CheckedOnColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(223)))), ((int)(((byte)(0)))));
+            this.chk_Remme.ForeColor = System.Drawing.Color.White;
+            this.chk_Remme.Location = new System.Drawing.Point(212, 309);
+            this.chk_Remme.Name = "chk_Remme";
+            this.chk_Remme.Size = new System.Drawing.Size(20, 20);
+            this.chk_Remme.TabIndex = 14;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.DarkOrange;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Segoe UI Semilight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.Gray;
+            this.linkLabel1.Location = new System.Drawing.Point(202, 391);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(152, 21);
+            this.linkLabel1.TabIndex = 13;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Forgotten Password?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(87, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 30);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(87, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 30);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Username";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(502, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Human Resources Management System";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SADProject2.Properties.Resources.National_Savings_Bank_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(166, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(226, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_Min
+            // 
+            this.btn_Min.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Min.Image = global::SADProject2.Properties.Resources.minimize;
+            this.btn_Min.ImageActive = null;
+            this.btn_Min.Location = new System.Drawing.Point(770, 19);
+            this.btn_Min.Name = "btn_Min";
+            this.btn_Min.Size = new System.Drawing.Size(20, 21);
+            this.btn_Min.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btn_Min.TabIndex = 4;
+            this.btn_Min.TabStop = false;
+            this.btn_Min.Zoom = 10;
+            this.btn_Min.Click += new System.EventHandler(this.btn_Min_Click);
+            // 
+            // bunifuImageButton1
+            // 
+            this.bunifuImageButton1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton1.Image = global::SADProject2.Properties.Resources.help;
+            this.bunifuImageButton1.ImageActive = null;
+            this.bunifuImageButton1.Location = new System.Drawing.Point(796, 19);
+            this.bunifuImageButton1.Name = "bunifuImageButton1";
+            this.bunifuImageButton1.Size = new System.Drawing.Size(20, 21);
+            this.bunifuImageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton1.TabIndex = 3;
+            this.bunifuImageButton1.TabStop = false;
+            this.bunifuImageButton1.Zoom = 10;
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Close.Image = global::SADProject2.Properties.Resources.Close;
+            this.btn_Close.ImageActive = null;
+            this.btn_Close.Location = new System.Drawing.Point(822, 19);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(20, 21);
+            this.btn_Close.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btn_Close.TabIndex = 2;
+            this.btn_Close.TabStop = false;
+            this.btn_Close.Zoom = 10;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(212, 183);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(255, 20);
+            this.txtUser.TabIndex = 18;
+            this.txtUser.TextChanged += new System.EventHandler(this.txtUser_TextChanged);
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(212, 246);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(255, 20);
+            this.txtPwd.TabIndex = 19;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(850, 520);
+            this.Controls.Add(this.btn_Min);
+            this.Controls.Add(this.bunifuImageButton1);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_Min)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_Close)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Bunifu.Framework.UI.BunifuThinButton2 btn_Login;
+        private System.Windows.Forms.Label label4;
+        private Bunifu.Framework.UI.BunifuCheckbox chk_Remme;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private Bunifu.Framework.UI.BunifuImageButton btn_Close;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton1;
+        private Bunifu.Framework.UI.BunifuImageButton btn_Min;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.TextBox txtUser;
+    }
+}
+

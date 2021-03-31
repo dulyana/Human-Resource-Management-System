8ab9ed5be7edd6c50b9fee31a14750f956a08f46
Dulyana  Gunawardhana <67815837+dulyana@users.noreply.github.com>
2021-04-01 00:18:13 +0530

    Add files via upload

---
+++ CustomImageButton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyImageButton
+{
+    public partial class CustomImageButton: PictureBox
+    {
+        public CustomImageButton()
+        {
+            InitializeComponent();
+        }
+
+        private Image NormalImage;
+        private Image HoverImage;
+
+        public Image ImageNormal
+        {
+            get { return NormalImage; }
+            set { NormalImage = value; }
+        }
+
+        public Image ImageHover
+        {
+            get { return HoverImage; }
+            set { HoverImage = value; }
+        }
+
+        private void CustomImageButton_MouseHover(object sender, EventArgs e)
+        {
+            this.Image = HoverImage;
+        }
+
+        private void CustomImageButton_MouseLeave(object sender, EventArgs e)
+        {
+            this.Image = NormalImage;
+        }
+    }
+}
